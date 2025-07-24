ed3e874515092a1bc628eb091762b3cb16b2449d
Jordan Jacobs <jordan@arrissoftware.com>
2025-07-24 11:47:21 -0400

    Remove duplicate legal links from footer bottom
    - Removed Privacy Policy and Terms of Service links from footer bottom
    - These links are already in the Legal column
    - Simplified footer bottom to only show copyright text

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -53,17 +53,7 @@ export default function Footer() {
           ))}
         </div>
         <div className="mt-8 pt-8 border-t border-gray-800">
-          <div className="flex flex-col md:flex-row justify-between items-center">
-            <p className="text-gray-400">© 2025 Martin Digital, Inc. All rights reserved.</p>
-            <div className="flex space-x-6 mt-4 md:mt-0">
-              <Link href="/privacy" className="text-gray-400 hover:text-white">
-                Privacy Policy
-              </Link>
-              <Link href="/terms-of-use" className="text-gray-400 hover:text-white">
-                Terms of Service
-              </Link>
-            </div>
-          </div>
+          <p className="text-gray-400">© 2025 Martin Digital, Inc. All rights reserved.</p>
         </div>
       </div>
     </footer>
